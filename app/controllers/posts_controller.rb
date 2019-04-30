697c600a791e7b9ca89ffc926684554a5fce56fe
Mallory Woods <malvwoods@gmail.com>
2019-04-29 21:08:00 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,6 +29,7 @@ class PostsController < ApplicationController
 	end
 
 	private
+	
 	def post_params
 		params.require(:post).permit(:title, :description, :post_status, :author_id, :category_id)
 	end
